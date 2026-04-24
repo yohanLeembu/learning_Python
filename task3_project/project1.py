75c5dc36c8813717ec816779a46e24e624181bf0
yohanLeembu <limbujohn307@gmail.com>
2026-04-24 14:54:09 +0545

    beta 2

--- task3_project/project1.py
+++ task3_project/project1.py
@@ -3,19 +3,21 @@ class Character:
     def __init__(self, name, health_points):
         self.name = name
         self.health_points = health_points
+        self.Max_health = self.health_points
 
     def attack (self, other):
         other.health_points -= 2
         return other.health_points
+    
+    def display_stats (self):
+        return f"{self.name} : {self.Max_health}/{self.health_points} "
 
 
 class Enemy(Character):
     pass
-        
 
 class Player(Character):
     pass
-
 
 player1 = Player("Yohan", 12)
 
@@ -34,9 +36,17 @@ while(enemy1.health_points > 0 and player1.health_points > 0 ):
         print(f"{enemy1.name} took 2 damamge!!")
         enemy1.attack(player1)
 
+    if action == 2:
+        print(player1.display_stats())
+
+    if action == 3:
+        break
+
 
 if enemy1.health_points == 0:
     print("You won!!")
 
-else:
+elif player1.health_points == 0:
     print("You Lost...")
+
+print("Thank you for Playing")
