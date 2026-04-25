a64f6aa5fcc3d3b6b430cf75656b93219aee7e8f
yohanLeembu <limbujohn307@gmail.com>
2026-04-25 23:28:27 +0545

    04-25 update

--- task3_project/main.py
+++ task3_project/main.py
@@ -1,5 +1,6 @@
-from base import Player, enemy_list, enemies
+from base import Player, enemy_list, enemies, encounter, Enemy
 from colorama import Fore, Style
+import time
 
 print("🐉⚔️ WELCOME TO THE EXTRAORDINARY DUNGEON⚔️🐉")
 
@@ -19,6 +20,30 @@ print(f"""
 """)
 
 choose = int(input("enter the number: "))
+
+if choose == 1:
+      print("👑Dungeon Administrator: Good Luck! Adventurer")
+      print("🔍 Exploring Dungeon...")
+
+      time.sleep(2)
+
+      enemy_encountered = encounter(name, enemies)
+      full_hp = enemy_encountered.hp
+      while name.hp >0 or enemy_encountered.hp >0:
+            print("\nwhat will you do?")
+            print(f"""
+                  1. 🗡️ {Fore.RED}Fight{Style.RESET_ALL}
+                  2. STATUS 📊 
+                  3. CHECK INVENTORY 🎒
+                  """)
+            action = int(input("Choose: "))
+            if action == 1:
+                  print(name.attack(enemy_encountered))
+                  time.sleep(1.5)
+                  print("\n",enemy_encountered.attack(name))
+                  time.sleep(1.5)
+
+
 
 if choose == 4:
     enemy_list(enemies)
